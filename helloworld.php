5bd847e450f7b23a01a2e671de8db73422f8652f
Michael <Michael@zenzet.com>
2017-08-07 09:56:47 +0800

    helloworld

---
+++ helloworld.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+	<?php
+		echo "'hello world";
+		$member['name'] = 'michael';
+		$st_so_powerful = 'good';
+		strtoupper($st_so_powerful)
+	?>
+	<?php echo "string";
+	if (true) {
+	 	echo "xxx";
+	 } ?>
+	 <?php if (condition): ?>
+	 	
+	 <?php else: ?>
+	 	
+	 <?php endif ?>
+
+
+	<?php print_r($_SERVER);
+	?>
+</body>
+</html>
